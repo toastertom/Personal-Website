c6dee1d9aeca54e5a566a5880d488219129946e2
ToasterTom <tate.thomas5595@gmail.com>
2016-10-23 22:49:11 -0600

    Everything Connected

---
+++ js/Directives/mainHeader.js
@@ -0,0 +1,33 @@
+var app = angular.module('profile');
+
+app.directive('mainHeader', function
+() {
+  return {
+    restrict: 'E',
+    templateUrl: '../../views/mainHeader.html',
+    transclude: true,
+    link: function () {
+
+    },
+    controller: function ($scope) {
+      $scope.navItems = [
+        {
+					name: 'ABOUT',
+					hash: '#/#about'
+				},
+				{
+					name: 'SKILLS',
+					hash: '#/#skills'
+				},
+				{
+					name: 'PORTFOLIO',
+					hash: '#/#portfolio'
+				},
+				{
+					name: 'CONTACT',
+					hash: '#/#contact'
+				}
+      ]
+    }
+  }
+})
---
+++ js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('profile', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: '../views/mainContent.html'
+  })
+  .otherwise('/');
+})
