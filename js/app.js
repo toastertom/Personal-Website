1754dddb077a810f9d34387a953b9e95c7fbf9aa
ToasterTom <tate.thomas5595@gmail.com>
2016-11-15 20:43:33 -0700

    testing something

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('profile', ['ngRoute']);
 app.config(function ($routeProvider) {
   $routeProvider
   .when('/test', {
-    templateUrl: '/views/mainContent.html'
+    templateUrl: '../views/mainContent.html'
   })
   .otherwise('/');
 })
