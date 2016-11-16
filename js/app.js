40258f4d5679fae9713629337d149b6b1359c12b
ToasterTom <tate.thomas5595@gmail.com>
2016-11-15 20:46:29 -0700

    move app.js to root directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('profile', ['ngRoute']);
-
-app.config(function ($routeProvider) {
-  $routeProvider
-  .when('/test', {
-    templateUrl: '../views/mainContent.html'
-  })
-  .otherwise('/');
-})
