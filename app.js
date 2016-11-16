bfdd3a111dee67c2959684d3c170090aba149ce3
ToasterTom <tate.thomas5595@gmail.com>
2016-11-15 20:58:33 -0700

    git hub working

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = angular.module('profile', ['ngRoute']);
 
 app.config(function ($routeProvider) {
   $routeProvider
-  .when('/test', {
+  .when('/', {
     templateUrl: './views/mainContent.html'
   })
   .otherwise('/');
