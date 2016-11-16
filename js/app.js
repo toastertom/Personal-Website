ea5c42ef6b570ee724fbd6c158ec5e8d871b11de
ToasterTom <tate.thomas5595@gmail.com>
2016-11-15 20:37:33 -0700

    ng router

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ var app = angular.module('profile', ['ngRoute']);
 
 app.config(function ($routeProvider) {
   $routeProvider
-  .state('/', {
-    templateUrl: '../views/mainContent.html'
+  .when('/', {
+    templateUrl: '/views/mainContent.html'
   })
   .otherwise('/');
 })
